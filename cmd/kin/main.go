b77152d5908db92379e7d706658b0700dd1eea07
Vincent Desloover <vincent.desloover.dev@gmail.com>
2024-03-22 17:04:24 -0400

    feat: add tsh interactions

--- cmd/kin/main.go
+++ cmd/kin/main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/charmbracelet/huh"
 )
 
-func main() {
-	// mock data
-	kubernetesClusters := []string{
-		"polaris-pre-prod",
-		"polaris-prod",
-	}
-	var selectedCluster string
+var selectedCluster string
 
+func main() {
+	kubernetesClusters := []string{}
+
+	// Login to the teleport proxy
+	tshLogin := exec.Command(
+		"tsh",
+		"login",
+		"--proxy=snyk.teleport.sh:443",
+		"snyk.teleport.sh",
+	)
+	loginOutput, err := tshLogin.Output()
+	if err != nil {
+		slog.Error("Failed to run the Teleport login command")
+		os.Exit(1)
+	}
+	fmt.Printf("%v\n", string(loginOutput))
+
+	// List kubernetes clusters
+	tshCmd := exec.Command("tsh", "kube", "ls")
+	listOutput, err := tshCmd.Output()
+	if err != nil {
+		slog.Error("Failed to run the Teleport list command")
+		os.Exit(1)
+	}
+
+	lines := strings.Split(string(listOutput), "\n")
+
+	// Remove headers
+	entries := lines[2:]
+	
+	// Extract available clusters
+	for _, entry := range entries {
+		clusterName := strings.Split(entry, " ")[0]
+		if clusterName != "" {
+			kubernetesClusters = append(kubernetesClusters, clusterName)
+		}
+	}
+
+	// Build user selection options
 	selectOptions := []huh.Option[string]{}
 	for _, cluster := range kubernetesClusters {
 		option := huh.NewOption(cluster, cluster)
@@ -32,7 +68,7 @@ func main() {
 		),
 	)
 
-	err := form.Run()
+	err = form.Run()
 	if err != nil {
 		slog.Error("Failed to run the CLI form")
 		os.Exit(1)
@@ -42,7 +78,13 @@ func main() {
 		slog.Warn("Invalid cluster selection")
 	}
 
-	slog.Info("Selection received", "cluster:", selectedCluster)
-	
-	return
+	clusterLoginCmd := exec.Command("tsh", "kube", "login", selectedCluster)
+	clusterLoginOut, err := clusterLoginCmd.Output()
+	if err != nil {
+		slog.Error("Failed to run the Teleport cluster login command")
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v\n", string(clusterLoginOut))
+	os.Exit(0)
 }
